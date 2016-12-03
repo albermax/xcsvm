c5b13c06ebac88d2ac24d3e8d68c229fa1c1857b
Max Alber <alber.maximilian@gmail.com>
2016-12-03 14:20:13 +0100

    Inital python package commit.

---
+++ xcsvm/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='xcsvm',
+      version='0.1',
+      description='Support Vector Machines for Extreme Classfication tasks.',
+      url='http://github.com/albermax/xcsvm',
+      author='Maxmilian Alber',
+      author_email='albermax@github.com',
+      license='MIT',
+      packages=['xcsvm'],
+      zip_safe=False)
